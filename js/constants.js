3cc906e615bae5654050e4d7fd2781867f89e059
Shinjuku <33587077+Shinjuku1@users.noreply.github.com>
2025-09-07 10:38:18 -0400

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@
  * This file (js/constants.js) holds static game data and constants
  * that are shared across multiple modules.
  */
-import { recipeData } from '/js/data/recipes.js';
+import { recipeData } from '/SatisfiedVisual/js/data/recipes.js';
 
 // Defines the maximum number of Somersloops that can be installed in each building type.
 export const SOMERSLOOP_SLOTS = {
@@ -20,4 +20,5 @@ export const SOMERSLOOP_SLOTS = {
 
 // Creates a Set of all building names that are categorized as 'Power' generators.
 // This allows for quick lookups to determine if a building produces or consumes power.
+
 export const GENERATOR_BUILDINGS = new Set(recipeData.buildings.filter(b => b.category === 'Power').map(b => b.name));
